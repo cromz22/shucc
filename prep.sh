1b34d1162dbcb0b3c42822a0786cf8571a877df0
cromz22 <tt.pfr4343@gmail.com>
2020-10-18 14:19:15 +0900

    rewrite prep.sh

--- prep.sh
+++ prep.sh
@@ -1,78 +1,77 @@
 #!/usr/bin/env bash
 
-# here=$(dirname $0)
-# test_file=${here}/test/test.c
+declare -a inputs
+declare -a outputs
 
-# tests=("return 0;" "return 42;" "return 5+20-4;")
-# answers=(0 42 21)
-declare -A tests=(
-  ["return 0;"]="0"
-  ["return 42;"]="42"
-  ["return 5+20-4;"]="21"
-  ["return 10 + 40 - 8;"]="42"
-  ["return 5 + 6 * 7;"]="47"
-  ["return 5 * (9 - 6);"]="15"
-  ["return (3 + 5) / 2;"]="4"
-  ["return -10 + 20;"]="10"
-  ["return 42 < 42;"]="0"
-  ["return 42 <= 42;"]="1"
-  ["return 42 > 42;"]="0"
-  ["return 42 >= 42;"]="1"
-  ["return 42 == 42;"]="1"
-  ["return 42 != 42;"]="0"
-  ["int a; return 14;"]="14"
-  ["int a; a = 3; return 42;"]="42"
-  ["int a; a = 3; int b; b = 5 * 6 - 8; return a + b / 2;"]="14"
-  ["int foo; int bar; foo = 3; bar = 5; return foo + bar;"]="8"
-  ["int foo; int bar; foo = 3; bar = 5; return foo + bar;"]="8"
-  ["if (1) return 42;"]="42"
-  ["if (1) return 42; else return 43;"]="42"
-  ["if (0) return 42; else return 43;"]="43"
-  ["if (1) if (1) return 42; else return 43;"]="42"
-  ["int foo; int bar; int baz; foo = 3; bar = 5; baz = 8; if (foo == 1) return foo + bar; if (foo == 2) return bar + baz; else return baz + foo;"]="11"
-  ["int foo; foo = 3; while(foo > 0) foo = foo - 1; return foo;"]="0"
-  ["int foo; foo = 3; while(foo > 0) if (foo == 2) return foo; else foo = foo - 1; return foo;"]="2"
-  ["int i; for (i = 0; i < 5; i = i + 1) i = i + 1; return i;"]="6"
-  ["int a; a = 1; if(a == 1) { a = a + 1; return a; }"]="2"
-  ["return foo();"]="3"
-  ["return add(2, 3);"]="5"
-  ["return fibo(5);"]="8"
-  ["int x; x = 3; int *y; y = &x; return *y;"]="3"
-  ["int *****x; return 5;"]="5"
-  ["int x; int *y; y = &x; *y = 3; return x;"]="3"
-  ["int a; int b; int c; a = 1; b = 2; c = 3; int *p; p = &c; int *q; q = p + 2; return *q;"]="1"
-  ["int a; int b; a = 1; b = 2; int *p; p = &a; int *q; q = p - 1; return *q;"]="2"
-  ["int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &c; int *q; q = p - 1; return *q;"]="4"
-  ["int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &d + 2; return *p;"]="2"
-  ["int x; return sizeof(x);"]="4"
-  ["int *y; return sizeof(y);"]="8"
-  ["return sizeof(1);"]="4"
-  ["return sizeof(sizeof(1));"]="4"
-  ["int a[10]; return 42;"]="42"
-  ["int a[3]; return sizeof(a);"]="12"
-  ["int *a[3]; return sizeof(a);"]="24"
-  ["int a[1]; *a = 3; return *a;"]="3"
-  ["int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1);"]="3"
-  ["int a[2]; *a = 1; return a[0];"]="1"
-  ["int a[2]; *a = 1; *(a + 1) = 2; return a[0] + a[1];"]="3"
-  ["int a[2]; *a = 1; *(a + 1) = 2; return 0[a] + 1[a];"]="3"
-  ["gvar = 42; return gvar;"]="42"
-  ["garr[1] = 42; return garr[1];"]="42"
-  ["gvar = 0; while (gvar < 3) {increment();} return gvar;"]="3"
-  ["char x; x = 1; return x;"]="1"
-  ["char x; return sizeof(x);"]="1"
-  ["int x[2][2]; return 0;"]="0"
-  ["int x[2][3]; int *y; y=x[0]; y[0]=0; return x[0][0];"]="0"
-  ["int x[2][3]; int *y; y=x[0]; y[1]=1; return x[0][1];"]="1"
-  ["int x[2][3]; int *y; y=x[0]; y[2]=2; return x[0][2];"]="2"
-  ["int x[2][3]; int *y; y=x[1]; y[0]=3; return x[1][0];"]="3"
-  ["int x[2][3]; int *y; y=x[1]; y[1]=4; return x[1][1];"]="4"
-  ["int x[2][3]; int *y; y=x[1]; y[2]=5; return x[1][2];"]="5"
-  ["int a[2][3]; return sizeof(*a);"]="12"
-#   ['char* foo; foo = "bar"; return 0;']="0"
-#   ['return first("bcd") - first("abc");']="1"
-#   ['char* foo; foo = "bar"; return 0;']="0"
-)
+append() {
+  inputs+=( "$1" )
+  outputs+=( "$2" )
+}
+
+append "return 0;" "0"
+append "return 42;" "42"
+append "return 5+20-4;" "21"
+append "return 10 + 40 - 8;" "42"
+append "return 5 + 6 * 7;" "47"
+append "return 5 * (9 - 6);" "15"
+append "return (3 + 5) / 2;" "4"
+append "return -10 + 20;" "10"
+append "return 42 < 42;" "0"
+append "return 42 <= 42;" "1"
+append "return 42 > 42;" "0"
+append "return 42 >= 42;" "1"
+append "return 42 == 42;" "1"
+append "return 42 != 42;" "0"
+append "int a; return 14;" "14"
+append "int a; a = 3; return 42;" "42"
+append "int a; a = 3; int b; b = 5 * 6 - 8; return a + b / 2;" "14"
+append "int foo; int bar; foo = 3; bar = 5; return foo + bar;" "8"
+append "int foo; int bar; foo = 3; bar = 5; return foo + bar;" "8"
+append "if (1) return 42;" "42"
+append "if (1) return 42; else return 43;" "42"
+append "if (0) return 42; else return 43;" "43"
+append "if (1) if (1) return 42; else return 43;" "42"
+append "int foo; int bar; int baz; foo = 3; bar = 5; baz = 8; if (foo == 1) return foo + bar; if (foo == 2) return bar + baz; else return baz + foo;" "11"
+append "int foo; foo = 3; while(foo > 0) foo = foo - 1; return foo;" "0"
+append "int foo; foo = 3; while(foo > 0) if (foo == 2) return foo; else foo = foo - 1; return foo;" "2"
+append "int i; for (i = 0; i < 5; i = i + 1) i = i + 1; return i;" "6"
+append "int a; a = 1; if(a == 1) { a = a + 1; return a; }" "2"
+append "return foo();" "3"
+append "return add(2, 3);" "5"
+append "return fibo(5);" "8"
+append "int x; x = 3; int *y; y = &x; return *y;" "3"
+append "int *****x; return 5;" "5"
+append "int x; int *y; y = &x; *y = 3; return x;" "3"
+append "int a; int b; int c; a = 1; b = 2; c = 3; int *p; p = &c; int *q; q = p + 2; return *q;" "1"
+append "int a; int b; a = 1; b = 2; int *p; p = &a; int *q; q = p - 1; return *q;" "2"
+append "int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &c; int *q; q = p - 1; return *q;" "4"
+append "int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &d + 2; return *p;" "2"
+append "int x; return sizeof(x);" "4"
+append "int *y; return sizeof(y);" "8"
+append "return sizeof(1);" "4"
+append "return sizeof(sizeof(1));" "4"
+append "int a[10]; return 42;" "42"
+append "int a[3]; return sizeof(a);" "12"
+append "int *a[3]; return sizeof(a);" "24"
+append "int a[1]; *a = 3; return *a;" "3"
+append "int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1);" "3"
+append "int a[2]; *a = 1; return a[0];" "1"
+append "int a[2]; *a = 1; *(a + 1) = 2; return a[0] + a[1];" "3"
+append "int a[2]; *a = 1; *(a + 1) = 2; return 0[a] + 1[a];" "3"
+append "gvar = 42; return gvar;" "42"
+append "garr[1] = 42; return garr[1];" "42"
+append "gvar = 0; while (gvar < 3) {increment();} return gvar;" "3"
+append "char x; x = 1; return x;" "1"
+append "char x; return sizeof(x);" "1"
+append "int x[2][2]; return 0;" "0"
+append "int x[2][3]; int *y; y=x[0]; y[0]=0; return x[0][0];" "0"
+append "int x[2][3]; int *y; y=x[0]; y[1]=1; return x[0][1];" "1"
+append "int x[2][3]; int *y; y=x[0]; y[2]=2; return x[0][2];" "2"
+append "int x[2][3]; int *y; y=x[1]; y[0]=3; return x[1][0];" "3"
+append "int x[2][3]; int *y; y=x[1]; y[1]=4; return x[1][1];" "4"
+append "int x[2][3]; int *y; y=x[1]; y[2]=5; return x[1][2];" "5"
+append "int a[2][3]; return sizeof(*a);" "12"
+
 
 helper=$(cat <<<"
 int gvar;
@@ -85,22 +84,27 @@ char first(char *str) { return str[0]; }
 ")
 echo "${helper}"
 
-i=0
-for prog in "${!tests[@]}"
-do
-  echo "int test${i}() { ${prog} }"
-  let i++
-done
+# i=0
+# for prog in "${!tests[@]}"
+# do
+#   echo "int test${i}() { ${prog} }"
+#   let i++
+# done
 echo "int assert(int expected, int actual, char *code);"
 
 echo ""
 
-echo "int main() {"
-i=0
-for test in "${!tests[@]}"
+for i in "${!inputs[@]}"
 do
-  echo "    assert(${tests[${test}]}, test${i}(), \"${test}\");"
-  let i++
+  echo "int test${i}() { ${inputs[i]} }"
+done
+
+echo ""
+
+echo "int main() {"
+for i in "${!inputs[@]}"
+do
+  echo "    assert(${outputs[i]}, test${i}(), \"${inputs[i]}\");"
 done
 echo "    return 0;"
 echo "}"
--- test/test.c
+++ test/test.c
@@ -6,132 +6,135 @@ int foo() { return 3; }
 int add(int a, int b) {return a + b;}
 int fibo(int n) { if (n == 1) return 1; else if (n == 0) return 1; else return fibo(n-1) + fibo(n-2); }
 char first(char *str) { return str[0]; }
-int test0() { if (0) return 42; else return 43; }
-int test1() { char x; return sizeof(x); }
-int test2() { int *a[3]; return sizeof(a); }
-int test3() { return (3 + 5) / 2; }
-int test4() { return 5+20-4; }
-int test5() { int x[2][3]; int *y; y=x[0]; y[0]=0; return x[0][0]; }
-int test6() { int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &d + 2; return *p; }
-int test7() { int x[2][3]; int *y; y=x[0]; y[2]=2; return x[0][2]; }
-int test8() { int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1); }
-int test9() { int *****x; return 5; }
-int test10() { return 42 <= 42; }
-int test11() { int x[2][3]; int *y; y=x[0]; y[1]=1; return x[0][1]; }
-int test12() { return 42 >= 42; }
-int test13() { int a; return 14; }
-int test14() { if (1) if (1) return 42; else return 43; }
-int test15() { int a[2]; *a = 1; *(a + 1) = 2; return 0[a] + 1[a]; }
-int test16() { return 10 + 40 - 8; }
-int test17() { return 42 != 42; }
-int test18() { int x[2][2]; return 0; }
-int test19() { int a[10]; return 42; }
-int test20() { garr[1] = 42; return garr[1]; }
-int test21() { int i; for (i = 0; i < 5; i = i + 1) i = i + 1; return i; }
-int test22() { int x[2][3]; int *y; y=x[1]; y[1]=4; return x[1][1]; }
-int test23() { gvar = 0; while (gvar < 3) {increment();} return gvar; }
-int test24() { int a[1]; *a = 3; return *a; }
-int test25() { int a[3]; return sizeof(a); }
-int test26() { int x; x = 3; int *y; y = &x; return *y; }
-int test27() { int a; a = 3; return 42; }
-int test28() { return add(2, 3); }
-int test29() { return 42; }
-int test30() { int foo; foo = 3; while(foo > 0) foo = foo - 1; return foo; }
-int test31() { int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &c; int *q; q = p - 1; return *q; }
-int test32() { int a; a = 1; if(a == 1) { a = a + 1; return a; } }
-int test33() { return fibo(5); }
-int test34() { return sizeof(1); }
-int test35() { int x; return sizeof(x); }
-int test36() { return 5 + 6 * 7; }
-int test37() { return 42 > 42; }
-int test38() { int a[2][3]; return sizeof(*a); }
-int test39() { char x; x = 1; return x; }
-int test40() { return sizeof(sizeof(1)); }
-int test41() { int foo; foo = 3; while(foo > 0) if (foo == 2) return foo; else foo = foo - 1; return foo; }
-int test42() { return 0; }
-int test43() { if (1) return 42; }
-int test44() { int x[2][3]; int *y; y=x[1]; y[0]=3; return x[1][0]; }
-int test45() { int a; int b; int c; a = 1; b = 2; c = 3; int *p; p = &c; int *q; q = p + 2; return *q; }
-int test46() { gvar = 42; return gvar; }
-int test47() { return 42 < 42; }
-int test48() { int a[2]; *a = 1; *(a + 1) = 2; return a[0] + a[1]; }
-int test49() { return 5 * (9 - 6); }
-int test50() { int foo; int bar; int baz; foo = 3; bar = 5; baz = 8; if (foo == 1) return foo + bar; if (foo == 2) return bar + baz; else return baz + foo; }
-int test51() { int a; a = 3; int b; b = 5 * 6 - 8; return a + b / 2; }
-int test52() { int foo; int bar; foo = 3; bar = 5; return foo + bar; }
-int test53() { int *y; return sizeof(y); }
-int test54() { int a; int b; a = 1; b = 2; int *p; p = &a; int *q; q = p - 1; return *q; }
-int test55() { int a[2]; *a = 1; return a[0]; }
-int test56() { return 42 == 42; }
-int test57() { int x[2][3]; int *y; y=x[1]; y[2]=5; return x[1][2]; }
-int test58() { return -10 + 20; }
-int test59() { if (1) return 42; else return 43; }
-int test60() { return foo(); }
-int test61() { int x; int *y; y = &x; *y = 3; return x; }
 int assert(int expected, int actual, char *code);
 
+int test0() { return 0; }
+int test1() { return 42; }
+int test2() { return 5+20-4; }
+int test3() { return 10 + 40 - 8; }
+int test4() { return 5 + 6 * 7; }
+int test5() { return 5 * (9 - 6); }
+int test6() { return (3 + 5) / 2; }
+int test7() { return -10 + 20; }
+int test8() { return 42 < 42; }
+int test9() { return 42 <= 42; }
+int test10() { return 42 > 42; }
+int test11() { return 42 >= 42; }
+int test12() { return 42 == 42; }
+int test13() { return 42 != 42; }
+int test14() { int a; return 14; }
+int test15() { int a; a = 3; return 42; }
+int test16() { int a; a = 3; int b; b = 5 * 6 - 8; return a + b / 2; }
+int test17() { int foo; int bar; foo = 3; bar = 5; return foo + bar; }
+int test18() { int foo; int bar; foo = 3; bar = 5; return foo + bar; }
+int test19() { if (1) return 42; }
+int test20() { if (1) return 42; else return 43; }
+int test21() { if (0) return 42; else return 43; }
+int test22() { if (1) if (1) return 42; else return 43; }
+int test23() { int foo; int bar; int baz; foo = 3; bar = 5; baz = 8; if (foo == 1) return foo + bar; if (foo == 2) return bar + baz; else return baz + foo; }
+int test24() { int foo; foo = 3; while(foo > 0) foo = foo - 1; return foo; }
+int test25() { int foo; foo = 3; while(foo > 0) if (foo == 2) return foo; else foo = foo - 1; return foo; }
+int test26() { int i; for (i = 0; i < 5; i = i + 1) i = i + 1; return i; }
+int test27() { int a; a = 1; if(a == 1) { a = a + 1; return a; } }
+int test28() { return foo(); }
+int test29() { return add(2, 3); }
+int test30() { return fibo(5); }
+int test31() { int x; x = 3; int *y; y = &x; return *y; }
+int test32() { int *****x; return 5; }
+int test33() { int x; int *y; y = &x; *y = 3; return x; }
+int test34() { int a; int b; int c; a = 1; b = 2; c = 3; int *p; p = &c; int *q; q = p + 2; return *q; }
+int test35() { int a; int b; a = 1; b = 2; int *p; p = &a; int *q; q = p - 1; return *q; }
+int test36() { int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &c; int *q; q = p - 1; return *q; }
+int test37() { int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &d + 2; return *p; }
+int test38() { int x; return sizeof(x); }
+int test39() { int *y; return sizeof(y); }
+int test40() { return sizeof(1); }
+int test41() { return sizeof(sizeof(1)); }
+int test42() { int a[10]; return 42; }
+int test43() { int a[3]; return sizeof(a); }
+int test44() { int *a[3]; return sizeof(a); }
+int test45() { int a[1]; *a = 3; return *a; }
+int test46() { int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1); }
+int test47() { int a[2]; *a = 1; return a[0]; }
+int test48() { int a[2]; *a = 1; *(a + 1) = 2; return a[0] + a[1]; }
+int test49() { int a[2]; *a = 1; *(a + 1) = 2; return 0[a] + 1[a]; }
+int test50() { gvar = 42; return gvar; }
+int test51() { garr[1] = 42; return garr[1]; }
+int test52() { gvar = 0; while (gvar < 3) {increment();} return gvar; }
+int test53() { char x; x = 1; return x; }
+int test54() { char x; return sizeof(x); }
+int test55() { int x[2][2]; return 0; }
+int test56() { int x[2][3]; int *y; y=x[0]; y[0]=0; return x[0][0]; }
+int test57() { int x[2][3]; int *y; y=x[0]; y[1]=1; return x[0][1]; }
+int test58() { int x[2][3]; int *y; y=x[0]; y[2]=2; return x[0][2]; }
+int test59() { int x[2][3]; int *y; y=x[1]; y[0]=3; return x[1][0]; }
+int test60() { int x[2][3]; int *y; y=x[1]; y[1]=4; return x[1][1]; }
+int test61() { int x[2][3]; int *y; y=x[1]; y[2]=5; return x[1][2]; }
+int test62() { int a[2][3]; return sizeof(*a); }
+
 int main() {
-    assert(43, test0(), "if (0) return 42; else return 43;");
-    assert(1, test1(), "char x; return sizeof(x);");
-    assert(24, test2(), "int *a[3]; return sizeof(a);");
-    assert(4, test3(), "return (3 + 5) / 2;");
-    assert(21, test4(), "return 5+20-4;");
-    assert(0, test5(), "int x[2][3]; int *y; y=x[0]; y[0]=0; return x[0][0];");
-    assert(2, test6(), "int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &d + 2; return *p;");
-    assert(2, test7(), "int x[2][3]; int *y; y=x[0]; y[2]=2; return x[0][2];");
-    assert(3, test8(), "int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1);");
-    assert(5, test9(), "int *****x; return 5;");
-    assert(1, test10(), "return 42 <= 42;");
-    assert(1, test11(), "int x[2][3]; int *y; y=x[0]; y[1]=1; return x[0][1];");
-    assert(1, test12(), "return 42 >= 42;");
-    assert(14, test13(), "int a; return 14;");
-    assert(42, test14(), "if (1) if (1) return 42; else return 43;");
-    assert(3, test15(), "int a[2]; *a = 1; *(a + 1) = 2; return 0[a] + 1[a];");
-    assert(42, test16(), "return 10 + 40 - 8;");
-    assert(0, test17(), "return 42 != 42;");
-    assert(0, test18(), "int x[2][2]; return 0;");
-    assert(42, test19(), "int a[10]; return 42;");
-    assert(42, test20(), "garr[1] = 42; return garr[1];");
-    assert(6, test21(), "int i; for (i = 0; i < 5; i = i + 1) i = i + 1; return i;");
-    assert(4, test22(), "int x[2][3]; int *y; y=x[1]; y[1]=4; return x[1][1];");
-    assert(3, test23(), "gvar = 0; while (gvar < 3) {increment();} return gvar;");
-    assert(3, test24(), "int a[1]; *a = 3; return *a;");
-    assert(12, test25(), "int a[3]; return sizeof(a);");
-    assert(3, test26(), "int x; x = 3; int *y; y = &x; return *y;");
-    assert(42, test27(), "int a; a = 3; return 42;");
-    assert(5, test28(), "return add(2, 3);");
-    assert(42, test29(), "return 42;");
-    assert(0, test30(), "int foo; foo = 3; while(foo > 0) foo = foo - 1; return foo;");
-    assert(4, test31(), "int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &c; int *q; q = p - 1; return *q;");
-    assert(2, test32(), "int a; a = 1; if(a == 1) { a = a + 1; return a; }");
-    assert(8, test33(), "return fibo(5);");
-    assert(4, test34(), "return sizeof(1);");
-    assert(4, test35(), "int x; return sizeof(x);");
-    assert(47, test36(), "return 5 + 6 * 7;");
-    assert(0, test37(), "return 42 > 42;");
-    assert(12, test38(), "int a[2][3]; return sizeof(*a);");
-    assert(1, test39(), "char x; x = 1; return x;");
-    assert(4, test40(), "return sizeof(sizeof(1));");
-    assert(2, test41(), "int foo; foo = 3; while(foo > 0) if (foo == 2) return foo; else foo = foo - 1; return foo;");
-    assert(0, test42(), "return 0;");
-    assert(42, test43(), "if (1) return 42;");
-    assert(3, test44(), "int x[2][3]; int *y; y=x[1]; y[0]=3; return x[1][0];");
-    assert(1, test45(), "int a; int b; int c; a = 1; b = 2; c = 3; int *p; p = &c; int *q; q = p + 2; return *q;");
-    assert(42, test46(), "gvar = 42; return gvar;");
-    assert(0, test47(), "return 42 < 42;");
+    assert(0, test0(), "return 0;");
+    assert(42, test1(), "return 42;");
+    assert(21, test2(), "return 5+20-4;");
+    assert(42, test3(), "return 10 + 40 - 8;");
+    assert(47, test4(), "return 5 + 6 * 7;");
+    assert(15, test5(), "return 5 * (9 - 6);");
+    assert(4, test6(), "return (3 + 5) / 2;");
+    assert(10, test7(), "return -10 + 20;");
+    assert(0, test8(), "return 42 < 42;");
+    assert(1, test9(), "return 42 <= 42;");
+    assert(0, test10(), "return 42 > 42;");
+    assert(1, test11(), "return 42 >= 42;");
+    assert(1, test12(), "return 42 == 42;");
+    assert(0, test13(), "return 42 != 42;");
+    assert(14, test14(), "int a; return 14;");
+    assert(42, test15(), "int a; a = 3; return 42;");
+    assert(14, test16(), "int a; a = 3; int b; b = 5 * 6 - 8; return a + b / 2;");
+    assert(8, test17(), "int foo; int bar; foo = 3; bar = 5; return foo + bar;");
+    assert(8, test18(), "int foo; int bar; foo = 3; bar = 5; return foo + bar;");
+    assert(42, test19(), "if (1) return 42;");
+    assert(42, test20(), "if (1) return 42; else return 43;");
+    assert(43, test21(), "if (0) return 42; else return 43;");
+    assert(42, test22(), "if (1) if (1) return 42; else return 43;");
+    assert(11, test23(), "int foo; int bar; int baz; foo = 3; bar = 5; baz = 8; if (foo == 1) return foo + bar; if (foo == 2) return bar + baz; else return baz + foo;");
+    assert(0, test24(), "int foo; foo = 3; while(foo > 0) foo = foo - 1; return foo;");
+    assert(2, test25(), "int foo; foo = 3; while(foo > 0) if (foo == 2) return foo; else foo = foo - 1; return foo;");
+    assert(6, test26(), "int i; for (i = 0; i < 5; i = i + 1) i = i + 1; return i;");
+    assert(2, test27(), "int a; a = 1; if(a == 1) { a = a + 1; return a; }");
+    assert(3, test28(), "return foo();");
+    assert(5, test29(), "return add(2, 3);");
+    assert(8, test30(), "return fibo(5);");
+    assert(3, test31(), "int x; x = 3; int *y; y = &x; return *y;");
+    assert(5, test32(), "int *****x; return 5;");
+    assert(3, test33(), "int x; int *y; y = &x; *y = 3; return x;");
+    assert(1, test34(), "int a; int b; int c; a = 1; b = 2; c = 3; int *p; p = &c; int *q; q = p + 2; return *q;");
+    assert(2, test35(), "int a; int b; a = 1; b = 2; int *p; p = &a; int *q; q = p - 1; return *q;");
+    assert(4, test36(), "int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &c; int *q; q = p - 1; return *q;");
+    assert(2, test37(), "int a; int b; int c; int d; a = 1; b = 2; c = 3; d = 4; int *p; p = &d + 2; return *p;");
+    assert(4, test38(), "int x; return sizeof(x);");
+    assert(8, test39(), "int *y; return sizeof(y);");
+    assert(4, test40(), "return sizeof(1);");
+    assert(4, test41(), "return sizeof(sizeof(1));");
+    assert(42, test42(), "int a[10]; return 42;");
+    assert(12, test43(), "int a[3]; return sizeof(a);");
+    assert(24, test44(), "int *a[3]; return sizeof(a);");
+    assert(3, test45(), "int a[1]; *a = 3; return *a;");
+    assert(3, test46(), "int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1);");
+    assert(1, test47(), "int a[2]; *a = 1; return a[0];");
     assert(3, test48(), "int a[2]; *a = 1; *(a + 1) = 2; return a[0] + a[1];");
-    assert(15, test49(), "return 5 * (9 - 6);");
-    assert(11, test50(), "int foo; int bar; int baz; foo = 3; bar = 5; baz = 8; if (foo == 1) return foo + bar; if (foo == 2) return bar + baz; else return baz + foo;");
-    assert(14, test51(), "int a; a = 3; int b; b = 5 * 6 - 8; return a + b / 2;");
-    assert(8, test52(), "int foo; int bar; foo = 3; bar = 5; return foo + bar;");
-    assert(8, test53(), "int *y; return sizeof(y);");
-    assert(2, test54(), "int a; int b; a = 1; b = 2; int *p; p = &a; int *q; q = p - 1; return *q;");
-    assert(1, test55(), "int a[2]; *a = 1; return a[0];");
-    assert(1, test56(), "return 42 == 42;");
-    assert(5, test57(), "int x[2][3]; int *y; y=x[1]; y[2]=5; return x[1][2];");
-    assert(10, test58(), "return -10 + 20;");
-    assert(42, test59(), "if (1) return 42; else return 43;");
-    assert(3, test60(), "return foo();");
-    assert(3, test61(), "int x; int *y; y = &x; *y = 3; return x;");
+    assert(3, test49(), "int a[2]; *a = 1; *(a + 1) = 2; return 0[a] + 1[a];");
+    assert(42, test50(), "gvar = 42; return gvar;");
+    assert(42, test51(), "garr[1] = 42; return garr[1];");
+    assert(3, test52(), "gvar = 0; while (gvar < 3) {increment();} return gvar;");
+    assert(1, test53(), "char x; x = 1; return x;");
+    assert(1, test54(), "char x; return sizeof(x);");
+    assert(0, test55(), "int x[2][2]; return 0;");
+    assert(0, test56(), "int x[2][3]; int *y; y=x[0]; y[0]=0; return x[0][0];");
+    assert(1, test57(), "int x[2][3]; int *y; y=x[0]; y[1]=1; return x[0][1];");
+    assert(2, test58(), "int x[2][3]; int *y; y=x[0]; y[2]=2; return x[0][2];");
+    assert(3, test59(), "int x[2][3]; int *y; y=x[1]; y[0]=3; return x[1][0];");
+    assert(4, test60(), "int x[2][3]; int *y; y=x[1]; y[1]=4; return x[1][1];");
+    assert(5, test61(), "int x[2][3]; int *y; y=x[1]; y[2]=5; return x[1][2];");
+    assert(12, test62(), "int a[2][3]; return sizeof(*a);");
     return 0;
 }
